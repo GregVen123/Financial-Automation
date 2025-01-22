4cda9b80f6a6302c5a182b34e56e12eb24fec085
Gregory V <164355603+GregVen123@users.noreply.github.com>
2025-01-22 17:38:08 -0500

    Update Fama-script.py

--- Fama-French-Model/Fama-script.py
+++ Fama-French-Model/Fama-script.py
@@ -13,6 +13,7 @@ for i in to_float:
     to_flaot2.append(float(i))
 company["Close/Last"] = to_flaot2
 search = Fama[Fama["Date"] == 20150102]
+#fix this
 Starting_date = int(input("What trading date does your data start in in year#month#day# form: "))
 index_num = Fama[Fama["Date"] == Starting_date].index
 Fama = Fama.drop(index=range(0,index_num[0]))
@@ -20,16 +21,20 @@ New_mk = []
 for i in range(len(Fama["Mkt-RF"])):
     New_mk.append(Fama.iloc[i]["Mkt-RF"]/100)
 Fama["Mkt-RF"] = New_mk
-
 new_smb = []
 for i in range(len(Fama["SMB"])):
     new_smb.append(Fama.iloc[i]["SMB"]/100)
 Fama["SMB"] = new_smb
-
 new_hml = []
 for i in range(len(Fama["HML"])):
     new_hml.append(Fama.iloc[i]["HML"]/100)
 Fama["HML"] = new_hml
+
+tail = Fama.tail(1)
+tail = str(tail["Date"].values[0])
+tail = tail[:4] + "/" + tail[4:6] + "/" + tail[6:8]
+
+
 company = company.drop(index=range(0,33))
 #print(msft, Fama)
 def percent_change(prev, forw):
