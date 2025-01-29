0184213cf4d80cbca5078894a06c19eec2a4c837
Gregory V <164355603+GregVen123@users.noreply.github.com>
2025-01-28 21:39:13 -0500

    Update Fama-script.py

--- Fama-French-Model/Fama-script.py
+++ Fama-French-Model/Fama-script.py
@@ -33,9 +33,10 @@ Fama["HML"] = new_hml
 tail = Fama.tail(1)
 tail = str(tail["Date"].values[0])
 tail = tail[:4] + "/" + tail[4:6] + "/" + tail[6:8]
+lastdate = input("What is the last day of the Fama French Data? in mm/dd/yyyy form: ")
 
-
-company = company.drop(index=range(0,33))
+lastdate = company[company["Date"] == lastdate]
+company = company.drop(index=range(0,lastdate.index[0]))
 #print(msft, Fama)
 def percent_change(prev, forw):
     return (forw/prev)-1
@@ -48,13 +49,16 @@ cm_daily_change = np.around(cm_daily_change,5)
 reg = LinearRegression()
 #Xpart = np.array(Fama["Mkt-RF"])
 #Xpart = Xpart.reshape(-1,1)
-
 Xpart = Fama[["Mkt-RF","SMB","HML"]]
 Ypart= cm_daily_change
 reg.fit(Xpart,Ypart)
 company["Percent Change"] = cm_daily_change
 print(f'The Betas are in order of Market, SMB, MHL {reg.coef_}')
-
 print(f' The intercept is {reg.intercept_}')
 res = (min(company["Percent Change"]))
+
+Mkt = Fama["Mkt-RF"].mean()
+SMB = Fama["SMB"].mean()
+HML = Fama["HML"].mean()
+print(Mkt,SMB,HML)
 
